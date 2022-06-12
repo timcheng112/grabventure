de552767d62d9254927d2623589dc991b9a26971
SH dubU <wshihan@u.nus.edu>
2022-06-13 00:22:03 +0800

    Added recommendation features

--- src/pages/AdventurePlan/AdventurePlan.jsx
+++ src/pages/AdventurePlan/AdventurePlan.jsx
@@ -23,13 +23,19 @@ const AdventurePlan = ({ adventureSetting }) => {
       <Header name="Adventure Plan" />
       <Box marginX={5}>
         <Text fontSize="2xl">Activity List</Text>
-        <Box>
-          <Text>{adventureSetting.budget}</Text>
-          <Text>{adventureSetting.duration}</Text>
-          <Text>{adventureSetting.location}</Text>
+        <Box marginY={10}>
+          {/* <Text>{adventureSetting.budget}</Text> */}
+          {/* <Text>{adventureSetting.duration}</Text> */}
+          {/* <Text>{adventureSetting.location}</Text> */}
 
+          <Text>A relaxing coffee break at...</Text>
           <Box maxW="sm" borderWidth="2px" borderRadius="lg" overflow="hidden">
-            <Image src={"https://bit.ly/2Z4KKcF"} alt={"image here"} />
+            <Image
+              src={
+                "https://ieatandeat.sg/wp-content/uploads/2015/10/Dutch-colony-coffee-co-1.jpg"
+              }
+              alt={"image here"}
+            />
 
             <Box
               mt="1"
@@ -38,7 +44,9 @@ const AdventurePlan = ({ adventureSetting }) => {
               lineHeight="tight"
               noOfLines={2}
             >
-              NAME OF THE PLACE
+              Dutch Colony Coffee Co.
+              <br />
+              New Tech Park
             </Box>
             <Divider />
             <Box display="flex" mt="2" alignItems="center">
@@ -49,6 +57,89 @@ const AdventurePlan = ({ adventureSetting }) => {
                 ))}
               <Box as="span" ml="2" color="gray.600" fontSize="sm">
                 {adventureSetting.budget}
+              </Box>
+              <Box as="span" ml="2" color="gray.600" fontSize="sm">
+                Food & Beverages
+              </Box>
+            </Box>
+          </Box>
+        </Box>
+
+        <Box marginY={10}>
+          {/* <Text>{adventureSetting.budget}</Text> */}
+          {/* <Text>{adventureSetting.duration}</Text> */}
+          {/* <Text>{adventureSetting.location}</Text> */}
+
+          <Text>A nice cool dessert at...</Text>
+          <Box maxW="sm" borderWidth="2px" borderRadius="lg" overflow="hidden">
+            <Image
+              src={"https://www.addressguru.sg/images/1054124559.jpg"}
+              alt={"image here"}
+            />
+
+            <Box
+              mt="1"
+              fontWeight="semibold"
+              as="h4"
+              lineHeight="tight"
+              noOfLines={2}
+            >
+              Wimbly Lu - Jalan Riang
+            </Box>
+            <Divider />
+            <Box display="flex" mt="2" alignItems="center">
+              {Array(5)
+                .fill("")
+                .map((_, i) => (
+                  <StarIcon key={i} color={i < 5 ? "teal.500" : "gray.300"} />
+                ))}
+              <Box as="span" ml="2" color="gray.600" fontSize="sm">
+                {adventureSetting.budget}
+              </Box>
+              <Box as="span" ml="2" color="gray.600" fontSize="sm">
+                Cafe
+              </Box>
+            </Box>
+          </Box>
+        </Box>
+
+        <Box marginY={10}>
+          {/* <Text>{adventureSetting.budget}</Text> */}
+          {/* <Text>{adventureSetting.duration}</Text> */}
+          {/* <Text>{adventureSetting.location}</Text> */}
+
+          <Text>Indulge in some lesser known delights...</Text>
+          <Box maxW="sm" borderWidth="2px" borderRadius="lg" overflow="hidden">
+            <Image
+              src={
+                "https://c1.staticflickr.com/5/4197/34820527092_ed40fc76e9_b.jpg"
+              }
+              alt={"image here"}
+            />
+
+            <Box
+              mt="1"
+              fontWeight="semibold"
+              as="h4"
+              lineHeight="tight"
+              noOfLines={2}
+            >
+              Hai Wei Yuan BBQ
+              <br />
+              Chomp Chomp Food Centre
+            </Box>
+            <Divider />
+            <Box display="flex" mt="2" alignItems="center">
+              {Array(5)
+                .fill("")
+                .map((_, i) => (
+                  <StarIcon key={i} color={i < 3 ? "teal.500" : "gray.300"} />
+                ))}
+              <Box as="span" ml="2" color="gray.600" fontSize="sm">
+                {adventureSetting.budget}
+              </Box>
+              <Box as="span" ml="2" color="gray.600" fontSize="sm">
+                Food & Beverages
               </Box>
             </Box>
           </Box>
